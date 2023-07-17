0420159e2c02fa555fdb6489a3373c5a73e610bd
Einav Rivni <erivni@synamedia.com>
2023-07-17 14:28:51 +0300

    added timeShiftBufferDepthMargin calc for timeline based mpd

---
+++ test/manifests/dynamic-timeline-timeshiftbufferdepth-margin.js
@@ -0,0 +1,287 @@
+export const parsedManifest = {
+  allowCache: true,
+  discontinuityStarts: [],
+  duration: 0,
+  endList: false,
+  mediaGroups: {
+    'AUDIO': {
+      audio: {
+        eng: {
+          autoselect: true,
+          default: true,
+          language: 'eng',
+          playlists: [
+            {
+              attributes: {
+                'BANDWIDTH': 96000,
+                'CODECS': 'mp4a.40.2',
+                'NAME': 'video-cif-stream-9-201',
+                'PROGRAM-ID': 1
+              },
+              discontinuitySequence: 0,
+              discontinuityStarts: [],
+              endList: false,
+              mediaSequence: 0,
+              resolvedUri: '',
+              segments: [
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 0,
+                  presentationTime: 1688906048,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001544320000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001544320000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 1,
+                  presentationTime: 1688906054,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001544860000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001544860000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 2,
+                  presentationTime: 1688906060,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001545400000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001545400000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 3,
+                  presentationTime: 1688906066,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001545940000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001545940000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 4,
+                  presentationTime: 1688906072,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001546480000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001546480000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 5,
+                  presentationTime: 1688906078,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001547020000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001547020000.m4f'
+                },
+                {
+                  duration: 6,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 6,
+                  presentationTime: 1688906084,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001547560000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001547560000.m4f'
+                },
+                {
+                  duration: 6,
+                  last: true,
+                  map: {
+                    resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-9-201.mp4',
+                    uri: '1687888416/init-video-cif-stream-9-201.mp4'
+                  },
+                  number: 7,
+                  presentationTime: 1688906090,
+                  resolvedUri: 'http://example.com/1687888416/video-cif-stream-9-201-152001548100000.m4f',
+                  timeline: 0,
+                  uri: '1687888416/video-cif-stream-9-201-152001548100000.m4f'
+                }
+              ],
+              targetDuration: 6,
+              timeline: 0,
+              timelineStarts: [
+                {
+                  start: 0,
+                  timeline: 0
+                }
+              ],
+              uri: ''
+            }
+          ],
+          uri: ''
+        }
+      }
+    },
+    'CLOSED-CAPTIONS': {},
+    'SUBTITLES': {},
+    'VIDEO': {}
+  },
+  minimumUpdatePeriod: 6000,
+  playlists: [
+    {
+      attributes: {
+        'AUDIO': 'audio',
+        'BANDWIDTH': 5000000,
+        'CODECS': 'avc1.4d4028',
+        'FRAME-RATE': 25,
+        'NAME': 'video-cif-stream-1-101',
+        'PROGRAM-ID': 1,
+        'RESOLUTION': {
+          height: 1080,
+          width: 1920
+        },
+        'SUBTITLES': 'subs'
+      },
+      discontinuitySequence: 0,
+      discontinuityStarts: [],
+      endList: false,
+      mediaSequence: 0,
+      resolvedUri: '',
+      segments: [
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 0,
+          presentationTime: 1688906048,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001544320000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001544320000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 1,
+          presentationTime: 1688906054,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001544860000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001544860000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 2,
+          presentationTime: 1688906060,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001545400000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001545400000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 3,
+          presentationTime: 1688906066,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001545940000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001545940000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 4,
+          presentationTime: 1688906072,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001546480000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001546480000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 5,
+          presentationTime: 1688906078,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001547020000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001547020000.m4f'
+        },
+        {
+          duration: 6,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 6,
+          presentationTime: 1688906084,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001547560000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001547560000.m4f'
+        },
+        {
+          duration: 6,
+          last: true,
+          map: {
+            resolvedUri: 'http://example.com/1687888416/init-video-cif-stream-1-101.mp4',
+            uri: '1687888416/init-video-cif-stream-1-101.mp4'
+          },
+          number: 7,
+          presentationTime: 1688906090,
+          resolvedUri: 'http://example.com/1687888416/video-cif-stream-1-101-152001548100000.m4f',
+          timeline: 0,
+          uri: '1687888416/video-cif-stream-1-101-152001548100000.m4f'
+        }
+      ],
+      targetDuration: 6,
+      timeline: 0,
+      timelineStarts: [
+        {
+          start: 0,
+          timeline: 0
+        }
+      ],
+      uri: ''
+    }
+  ],
+  publishTime: '2023-07-09T12:34:56.000Z',
+  segments: [],
+  suggestedPresentationDelay: undefined,
+  timeShiftBufferDepth: 60,
+  timeShiftBufferDepthMargin: 10,
+  timelineStarts: [
+    {
+      start: 0,
+      timeline: 0
+    }
+  ],
+  uri: ''
+};
