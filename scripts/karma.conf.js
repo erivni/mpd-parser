a276805e23cb11b0273ea3acf748d40db98ef31c
Einav Rivni <erivni@synamedia.com>
2025-05-25 17:32:18 +0300

    fix git action

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -7,7 +7,18 @@ module.exports = function(config) {
   const options = {};
 
   config = generate(config, options);
-
   // any other custom stuff not supported by options here!
+
+  config.customLaunchers = {
+    ChromeHeadlessNoSandbox: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox']
+    }
+  };
+
+  // Force usage of the custom launcher
+  config.browsers = ['ChromeHeadlessNoSandbox'];
+
+  return config;
 };
 
