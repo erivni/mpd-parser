043b9c252f4252ffb44d3fb6eb51ee035220a94d
Einav Rivni <erivni@synamedia.com>
2025-05-26 09:04:47 +0300

    karama upgrade

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -4,20 +4,19 @@ module.exports = function(config) {
 
   // see https://github.com/videojs/videojs-generate-karma-config
   // for options
-  const options = {};
-
-  config = generate(config, options);
-  // any other custom stuff not supported by options here!
-
-  config.customLaunchers = {
-    ChromeHeadlessNoSandbox: {
-      base: 'ChromeHeadless',
-      flags: ['--no-sandbox']
+  const options = {
+    customLaunchers(defaults) {
+      return Object.assign(defaults, {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+        }
+      });
     }
   };
 
-  // Force usage of the custom launcher
-  config.browsers = ['ChromeHeadlessNoSandbox'];
+  config = generate(config, options);
+  // any other custom stuff not supported by options here!
 
   return config;
 };
